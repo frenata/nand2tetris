2b5539ba19a4e22a4cc8505bfe4805bd8531de4e
Andrew Nichols <danweasel@gmail.com>
2017-08-28 14:47:30 +0700

    Write assembly program to multiply two numbers.

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,42 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+D=M
+@n
+M=D
+
+@R1
+D=M
+@m
+M=D
+
+@total
+M=0
+
+(LOOP)
+//check if m is <= 0
+@m
+D=M
+@OUTPUT
+D;JLE
+
+@n
+D=M
+@total
+M=M+D
+@m
+M=M-1
+
+@LOOP
+0;JMP
+
+(OUTPUT)
+@total
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
