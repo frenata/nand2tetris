ad955b1cecc4f76f86bf87ee0a10ec7d1da528c9
Andrew Nichols <danweasel@gmail.com>
2017-08-28 15:48:38 +0700

    Write assembly program to fill the screen black when a key is pressed.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,69 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@color
+M=0
+
+@8192
+D=A
+@rows
+M=D
+
+(WAIT)
+@KBD
+D=M
+@BLACK
+D;JNE
+@WHITE
+0;JMP
+
+(BLACK)
+@color
+D=M
+@WAIT
+D+1;JEQ // don't paint if color was already -1
+@color
+M=-1
+@PAINT
+0;JMP
+
+(WHITE)
+@color
+D=M
+@WAIT
+D;JEQ // don't paint if color was already 0
+@color
+M=0
+@PAINT
+0;JMP
+
+(PAINT)
+// count up rows from 0 and start row addresses from SCREEN root
+@i
+M=0
+@SCREEN
+D=A
+@current
+M=D
+
+(LOOP)
+// if rows - i == 0, stop
+@rows
+D=M
+@i
+D=D-M
+M=M+1
+@WAIT
+D;JEQ
+
+// paint the current row and increment it
+@color
+D=M
+@current
+A=M
+M=D
+@current
+M=M+1
+
+@LOOP
+0;JMP
