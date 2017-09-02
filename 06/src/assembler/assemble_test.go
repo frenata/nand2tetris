b244f7f3ef8a250d3510143a2b0fbc4cd6d2d90b
Andrew Nichols <danweasel@gmail.com>
2017-09-02 16:01:44 +0700

    Add tests for all symbol-less assembly files

--- 06/src/assembler/assemble_test.go
+++ 06/src/assembler/assemble_test.go
@@ -6,23 +6,29 @@ import (
 	"testing"
 )
 
+var testFiles map[string]string = map[string]string{
+	"../../tests/add/Add.asm":    "../../tests/add/Add.cmp",
+	"../../tests/max/MaxL.asm":   "../../tests/max/MaxL.cmp",
+	"../../tests/rect/RectL.asm": "../../tests/rect/RectL.cmp",
+	"../../tests/pong/PongL.asm": "../../tests/pong/PongL.cmp",
+}
+
 func TestAssemble(t *testing.T) {
-	asmFile := "../../tests/add/Add.asm"
-	cmpFile := "../../tests/add/Add.cmp"
+	for asmFile, cmpFile := range testFiles {
+		input, err := ioutil.ReadFile(asmFile)
+		if err != nil {
+			panic(err)
+		}
+		expected, err := ioutil.ReadFile(cmpFile)
+		if err != nil {
+			panic(err)
+		}
+		actual := assembler.Assemble(string(input))
 
-	input, err := ioutil.ReadFile(asmFile)
-	if err != nil {
-		panic(err)
-	}
-	expected, err := ioutil.ReadFile(cmpFile)
-	if err != nil {
-		panic(err)
-	}
-	actual := assembler.Assemble(string(input))
-
-	if actual != string(expected) {
-		t.Logf("Actual:\n%s", actual)
-		t.Logf("Expected:\n%s", expected)
-		t.Fatalf("Output of Assembler did not match the contents of %s", cmpFile)
+		if actual != string(expected) {
+			t.Logf("Actual:\n%s", actual)
+			t.Logf("Expected:\n%s", expected)
+			t.Fatalf("Output of Assembler did not match the contents of %s", cmpFile)
+		}
 	}
 }
