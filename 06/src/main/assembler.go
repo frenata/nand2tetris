dd9fb7b343d9682d4cf75093e349e7a595d32255
Andrew Nichols <danweasel@gmail.com>
2017-09-02 21:13:20 +0700

    Build basic binary to run assembler

---
+++ 06/src/main/assembler.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"assembler"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("No file specified")
+	}
+	filename := args[1]
+	ext := filepath.Ext(filename)
+	name := strings.TrimSuffix(filename, ext)
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	output := assembler.Assemble(string(file))
+	binary, err := os.Create(name + ".hack")
+	if err != nil {
+		panic(err)
+	}
+	defer binary.Close()
+
+	n, err := binary.WriteString(output)
+	if err != nil {
+		log.Printf("%d bytes written", n)
+		panic(err)
+	}
+}
